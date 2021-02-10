51e97b9a3f12f5e2c8798a05ae08ffce938c2f5e
nikolska <nikolska1995@gmail.com>
2021-02-10 15:27:13 +0100

    add search disease view

--- medical_app/main_app/views.py
+++ medical_app/main_app/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import View
 
 from .models import *
@@ -16,7 +16,7 @@ class OrgansListView(View):
     template = 'organs_list.html'
 
     def get(self, request):
-        organs = Organ.objects.order_by('name')
+        organs = get_list_or_404(Organ.objects.order_by('name'))
         ctx = {'organs': organs}
         return render(request, self.template, ctx)
 
@@ -25,7 +25,7 @@ class SymptomsListView(View):
     template = 'symptoms_list.html'
 
     def get(self, request):
-        symptoms = Symptom.objects.order_by('name')
+        symptoms = get_list_or_404(Symptom.objects.order_by('name'))
         ctx = {'symptoms': symptoms}
         return render(request, self.template, ctx)
 
@@ -34,7 +34,7 @@ class DiseasesListView(View):
     template = 'diseases_list.html'
 
     def get(self, request):
-        diseases = Disease.objects.order_by('name')
+        diseases = get_list_or_404(Disease.objects.order_by('name'))
         ctx = {'diseases': diseases}
         return render(request, self.template, ctx)
 
@@ -51,5 +51,34 @@ class DiseaseDetailsView(View):
 
 
 class SearchDiseaseView(View):
-    pass
+    template = 'search_disease.html'
+
+    def get(self, request):
+        symptoms = get_list_or_404(Symptom.objects.order_by('name'))
+        organs = get_list_or_404(Organ.objects.order_by('name'))
+        ctx = {'symptoms': symptoms,
+               'organs': organs}
+        return render(request, self.template, ctx)
+
+    def post(self, request):
+        symptoms = request.POST.getlist('symptoms')
+        organs = request.POST.getlist('affected_organs')
+
+        if organs:
+            try:
+                diseases = Disease.objects.filter(affected_organs__in=organs).distinct()
+            except:
+                raise Http404
+
+        if symptoms:
+            try:
+                diseases = Disease.objects.filter(symptoms__in=symptoms).distinct()
+            except:
+                raise Http404
+            
+        if not organs and not symptoms:
+            diseases = Disease.objects.order_by('name')
+
+        ctx = {'diseases': diseases}
+        return render(request, 'diseases_list.html', ctx)
 
