090c1cdfc4ae4fe5185cbd29155f89596b7a15e7
nikolska <nikolska1995@gmail.com>
2021-03-10 16:09:53 +0100

    update organs list view

--- medical_app/static/js/main.js
+++ medical_app/static/js/main.js
@@ -57,6 +57,32 @@ $(document).ready(function(){
   });
 });
 
+const links = document.querySelectorAll("a.image");
+const lightbox = document.querySelector(".lightbox");
+const lightboxCtn = document.querySelector(".lightbox-cnt");
+const img = document.querySelector(".lightbox-img");
+const button = document.querySelector(".lightbox-close");
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        img.src = link.href;
+        lightbox.style.display = 'flex';
+    })
+});
+
+button.addEventListener('click', e => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', e => {
+    lightbox.style.display = 'none';
+});
+
+lightboxCtn.addEventListener('click', e => {
+    e.stopPropagation();
+});
+
 // Add new disease: if input is checked -> check selected option -> save id data object.
 // const inputs = document.querySelectorAll("input.input-symptoms");
 // const select_list = document.querySelectorAll("select.select-frequency");
