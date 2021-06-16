9ccc22f0a575c635901149bc78580e2fd8dcf8cc
Anastazja Szyjko <72386975+nikolska@users.noreply.github.com>
2021-06-16 13:34:31 +0200

    Update settings.py

--- medical_app/settings.py
+++ medical_app/settings.py
@@ -118,10 +118,12 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nikolska.work@gmail.com'
-EMAIL_HOST_PASSWORD = 'Dontrepeatyourself48'
+EMAIL_HOST_PASSWORD = 'oqlisdbsdzddnlns'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Internationalization
